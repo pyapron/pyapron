4cb0ef07516ad0e1de630d5463f471222670bfbf
Remy Boutonnet <remy.boutonnet@protonmail.com>
2018-07-16 22:22:01 +0200

    adding patching of apron libraries with patchelf to add rpath

--- pyapron/apron_util.c
+++ pyapron/apron_util.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
+#include "apron/ap_tcons1.h"
 
-void tcons1_alloc()
+void tcons1_alloc(ap_constyp_t constyp,
+                  ap_texpr1_t * texpr1,
+                  ap_scalar_t * scalar)
 {
+    ap_tcons1_t * tcons1 = malloc(sizeof(ap_tcons1_t));
+    *tcons1 = ap_tcons1_make(constyp, texpr1, scalar);
 }
